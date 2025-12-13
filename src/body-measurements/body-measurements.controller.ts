add303f95432d66826351e36dc80df983d5a0c8f
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-12-13 15:13:50 +0100

    feat: add update and delete functionality for body measurements

--- src/body-measurements/body-measurements.controller.ts
+++ src/body-measurements/body-measurements.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { type AuthUser } from 'src/common/types/auth-user.interface';
 import { BodyMeasurementsService } from './body-measurements.service';
 import { CreateMeasurementDto } from './dtos/create-measurement.dto';
+import { UpdateMeasurementDto } from './dtos/update-measurement.dto';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
@@ -25,5 +36,29 @@ export class BodyMeasurementsController {
     @Body() body: CreateMeasurementDto,
   ) {
     return this.bodyMeasurementsService.createMeasurement(user.id, body);
+  }
+
+  @Patch(':measurementId')
+  updateMeasurement(
+    @CurrentUser() user: AuthUser,
+    @Param('measurementId', ParseIntPipe) measurementId: number,
+    @Body() body: UpdateMeasurementDto,
+  ) {
+    return this.bodyMeasurementsService.updateMeasurement(
+      user.id,
+      measurementId,
+      body,
+    );
+  }
+
+  @Delete(':measurementId')
+  deleteMeasurement(
+    @CurrentUser() user: AuthUser,
+    @Param('measurementId', ParseIntPipe) measurementId: number,
+  ) {
+    return this.bodyMeasurementsService.deleteMeasurement(
+      user.id,
+      measurementId,
+    );
   }
 }
--- src/body-measurements/body-measurements.service.ts
+++ src/body-measurements/body-measurements.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateMeasurementDto } from './dtos/create-measurement.dto';
+import { UpdateMeasurementDto } from './dtos/update-measurement.dto';
 
 @Injectable()
 export class BodyMeasurementsService {
@@ -19,6 +20,29 @@ export class BodyMeasurementsService {
 
     return this.prismaService.bodyMeasurement.create({
       data: { ...data, userId },
+    });
+  }
+
+  updateMeasurement(
+    userId: number,
+    measurementId: number,
+    data: UpdateMeasurementDto,
+  ) {
+    return this.prismaService.bodyMeasurement.update({
+      where: {
+        id: measurementId,
+        userId,
+      },
+      data,
+    });
+  }
+
+  deleteMeasurement(userId: number, measurementId: number) {
+    return this.prismaService.bodyMeasurement.delete({
+      where: {
+        id: measurementId,
+        userId,
+      },
     });
   }
 }
