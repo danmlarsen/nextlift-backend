cefc9b0c372897965dcc25e046327fbc87c6c0d5
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-12-13 13:01:30 +0100

    feat: implement BodyMeasurements controller and service with measurement creation and retrieval

--- src/body-measurements/body-measurements.controller.ts
+++ src/body-measurements/body-measurements.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { type AuthUser } from 'src/common/types/auth-user.interface';
+import { BodyMeasurementsService } from './body-measurements.service';
+import { CreateMeasurementDto } from './dtos/create-measurement.dto';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('body-measurements')
-export class BodyMeasurementsController {}
+export class BodyMeasurementsController {
+  constructor(
+    private readonly bodyMeasurementsService: BodyMeasurementsService,
+  ) {}
+
+  @Get()
+  getMeasurements(@CurrentUser() user: AuthUser) {
+    return this.bodyMeasurementsService.findMeasurements(user.id);
+  }
+
+  @Post()
+  createMeasurement(
+    @CurrentUser() user: AuthUser,
+    @Body() body: CreateMeasurementDto,
+  ) {
+    return this.bodyMeasurementsService.createMeasurement(user.id, body);
+  }
+}
--- src/body-measurements/body-measurements.service.ts
+++ src/body-measurements/body-measurements.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateMeasurementDto } from './dtos/create-measurement.dto';
 
 @Injectable()
-export class BodyMeasurementsService {}
+export class BodyMeasurementsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  findMeasurements(userId: number) {
+    return this.prismaService.bodyMeasurement.findMany({
+      where: { userId },
+    });
+  }
+
+  createMeasurement(userId: number, data: CreateMeasurementDto) {
+    if (!data.measuredAt) {
+      data.measuredAt = new Date().toISOString();
+    }
+
+    return this.prismaService.bodyMeasurement.create({
+      data: { ...data, userId },
+    });
+  }
+}
